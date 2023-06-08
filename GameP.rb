35a54797bc81b32d6e24cf823f45a2e06bbf39a2
Drileyf <drileyf1@gmail.com>
2023-06-07 22:23:26 -0700

    Update GameP.rb

--- GameP.rb
+++ GameP.rb
@@ -17,7 +17,7 @@ ball = Ball.new(ball_velocity)
 ball_trajectory = BallTrajectory.new(ball)
 scoreBoard = ScoreBoard.new(0,0)
 
-Slist = ['./SFX/We-are-Finally-Landing.mp3','./SFX/Feel-Good','./SFX/On-The-Prowl(rX).mp3','./SFX/music.wav',
+Slist = ['./SFX/We-are-Finally-Landing.mp3','./SFX/Feel-Good.mp3','./SFX/On-The-Prowl(rX).mp3','./SFX/music.wav',
     './SFX/Nuclear-Winter.mp3','./SFX/Fogger (R).mp3','./SFX/Mechanical-Man(R).mp3','./SFX/Valves.mp3','./SFX/Scouting.mp3',
     './SFX/Mutants.mp3','./SFX/Brimstone.mp3','./SFX/Brain-Freeze (R).mp3','./SFX/Grinder.mp3','./SFX/Hell-March(R).mp3',
     './SFX/Femme-Fatale.mp3','./SFX/Queen-of-Knives.mp3','./SFX/Electro-Cabello.mp3']

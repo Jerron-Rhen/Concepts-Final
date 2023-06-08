af6585b2adf5b614e0049c62c70e36490f65865e
Jerron-Rhen <jerron.rhen@oit.edu>
2023-06-07 21:55:19 -0700

    added random BGMs

--- GameP.rb
+++ GameP.rb
@@ -11,15 +11,20 @@ PING_SOUND = Sound.new('./SFX/ping.wav')
 
 ball_velocity = 6
 
-player = Paddle.new(:left, 8, 0, 200)
-opponent = Paddle.new(:right, 8, 0, 200)
+player = Paddle.new(:left, 8, 0, 285)
+opponent = Paddle.new(:right, 8, 0, 285)
 ball = Ball.new(ball_velocity)
 ball_trajectory = BallTrajectory.new(ball)
 scoreBoard = ScoreBoard.new(0,0)
 
-bmg = Music.new('./SFX/music.wav')
-bmg.loop = true
-bmg.play
+Slist = ['./SFX/We-are-Finally-Landing.mp3','./SFX/Feel-Good','./SFX/On-The-Prowl(rX).mp3','./SFX/music.wav',
+    './SFX/Nuclear-Winter.mp3','./SFX/Fogger (R).mp3','./SFX/Mechanical-Man(R).mp3','./SFX/Valves.mp3','./SFX/Scouting.mp3',
+    './SFX/Mutants.mp3','./SFX/Brimstone.mp3','./SFX/Brain-Freeze (R).mp3','./SFX/Grinder.mp3','./SFX/Hell-March(R).mp3',
+    './SFX/Femme-Fatale.mp3','./SFX/Queen-of-Knives.mp3','./SFX/Electro-Cabello.mp3']
+
+bgm = Music.new(Slist.sample)
+bgm.loop = true
+bgm.play
 
 last_hit_frame = 0
 
@@ -69,13 +74,13 @@ end
 
 on :key_held do |event|
     if event.key == 'w'
-    player.y_movement = -1
+      player.y_movement = -1
     elsif event.key == 's'
-    player.y_movement = 1
+      player.y_movement = 1
     elsif event.key == 'up'
-    opponent.y_movement = -1
+      opponent.y_movement = -1
     elsif event.key == 'down'
-    opponent.y_movement = 1
+      opponent.y_movement = 1
   end
 end
 
